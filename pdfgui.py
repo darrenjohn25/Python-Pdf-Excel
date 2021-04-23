efd795b47797aa675e22a1908a612d3c30958c85
Darren John Mathews Y G <67472616+darrenjohn25@users.noreply.github.com>
2021-04-23 12:02:05 +0530

    Gui Ui Changed

--- pdfgui.py
+++ pdfgui.py
@@ -1,20 +1,24 @@
 from tkinter import *
+import tabula
 from PIL import Image, ImageTk
 from tkinter import filedialog
 root = Tk()
-root.title("Tkinter Eg")
+canvas= Canvas(root,width=500,height=300)
+canvas.pack()
+root.title("Python Pdf-Excel")
+logo = ImageTk.PhotoImage(Image.open(r('Urlof anyimage')
+canvas.create_image(30,30,anchor=NW,image=logo)
 def open():
     global root.filename
     root.filename = filedialog.askopenfilename()
     my_label = Label(root,text=root.filename).pack()   
-    # my_image = ImageTk.PhotoImage(Image.open(root.filename))
-    # my_image_label = Label(image=my_image).pack()
 def convertxl():
     my_label = Label(root,text=root.filename).pack()
+    destination=filedialog.asksaveasfilename(defaultextension=".xlsx")                                 
     df = tabula.read_pdf(root.filename, lattice = True)[1]
     df.columns = df.columns.str.replace('\r', ' ')
     data = df.dropna()
-    data.to_excel('data.xlsx')
-my_btn = Button(root,text="Open File",command=open).pack()
-my_btn = Button(root,text="Convert to Excel",command=convertxl).pack()
+    data.to_excel(destination)
+my_btn = Button(root,text="Open File",command=open).pack(side=LEFT)
+mybtn = Button(root,text="Convert to Excel",command=convertxl).pack(side=LEFT)
 root = mainloop()
